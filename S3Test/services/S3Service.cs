3fb3760c7452fc8aad21371d4a02ef340d155e3e
Ramzan <rumkkhan@gmail.com>
2019-11-21 09:46:06 +0530

    fetch excel form s3

--- S3Test/services/S3Service.cs
+++ S3Test/services/S3Service.cs
@@ -3,6 +3,14 @@ using Amazon.S3.Model;
 using Amazon.S3.Transfer;
 using Amazon.S3.Util;
 using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using ExcelDataReader;
+using GemBox.Spreadsheet;
+using NPOI.HSSF.Model;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using OfficeOpenXml;
 using S3Test.Models;
 using System;
 using System.Collections.Generic;
@@ -16,6 +24,7 @@ namespace S3Test.services
     public class S3Service  : IS3Service
     {
         private readonly IAmazonS3 _client;
+        private InternalWorkbook stream;
 
         public S3Service(IAmazonS3 client)
         {
@@ -115,7 +124,7 @@ namespace S3Test.services
 
         public async Task GetObjectFromS3Async(string buckName)
         {
-            const string keyName = "Create.xlsx";
+            const string keyName = "ANX -1 Normal30knew.xls";
 
             try
             {
@@ -127,63 +136,37 @@ namespace S3Test.services
                 };
                 string responseBody;
                 byte[] data;
-                using (var response = await _client.GetObjectAsync(request))
-                using (var responseStream = response.ResponseStream)
+                ISheet sheet;
+                var countSheets = 0;
                
-                using (XLWorkbook workBook = new XLWorkbook(responseStream))
+                using (var response = await _client.GetObjectAsync(request))
+                using (Stream responseStream = response.ResponseStream)
+                using (MemoryStream memStream = new MemoryStream())
                 {
-                    //Read the first Sheet from Excel file.
-                    IXLWorksheet workSheet = workBook.Worksheet(1);
-
-                    //Create a new DataTable.
-                    DataTable dt = new DataTable();
-                    bool firstRow = true;
-                  
-                    foreach (IXLRow row in workSheet.Rows())
+                   
+                    responseStream.CopyTo(memStream);
+                    memStream.Seek(0, SeekOrigin.Begin);
+                    HSSFWorkbook hssfwb = new HSSFWorkbook(memStream);
+                     sheet = hssfwb.GetSheetAt(2);
+                    var gendral = hssfwb.GetSheetAt(0);
+                    IRow headerRowr = sheet.GetRow(7);
+                    int cellCountt = headerRowr.LastCellNum;
+                    countSheets = hssfwb.Count;
+                    //get first sheet from workbook  
+                    for (int i = 0; i < hssfwb.Count; i++)
                     {
-                        //Use the first row to add columns to DataTable.
-                        if (firstRow)
-                        {
-                            foreach (IXLCell cell in row.Cells())
-                            {
-                                dt.Columns.Add(cell.Value.ToString());
-                            }
-                            firstRow = false;
-                        }
-                        else
-                        {
-                            //Add rows to DataTable.
-                            dt.Rows.Add();
-                            int i = 0;
-                            foreach (IXLCell cell in row.Cells())
-                            {
-                                dt.Rows[dt.Rows.Count - 1][i] = cell.Value.ToString();
-                                i++;
-                            } 
-                        }
+                        sheet = hssfwb.GetSheetAt(i + 1);
+                        var Name = hssfwb.GetSheetName(i);//get sheet names
+                        IRow headerRow = sheet.GetRow(5); //Get Header Row
+                        int cellCount = headerRow.LastCellNum;
 
+                     var    abc = renderData(headerRow, cellCount, sheet);
                     }
-                    var aa = dt.Rows.Count;
 
-                    //    using (var reader = new StreamReader(responseStream))
-                    //{
-                    //    var title = response.Metadata["x-amz-meta-title"];
-                    //    var contentType = response.Headers["Content-Type"];
-                    //    var pathAndFileName = $"D:\\{keyName}";
-                    //    responseBody = reader.ReadToEnd();
-                    //    using (MemoryStream ms = new MemoryStream())
-                    //    {
-                    //        reader.BaseStream.CopyTo(ms);
-                    //        data = ms.ToArray();
-                    //    }
-                    //    File.WriteAllBytes("pathAndFileName", data);
-                    //    //byte[] bytes = reader;
-                    //}
-                    var pathAndFileNamee = $"D:\\{keyName}";
-                    var createText = "";
-                    File.WriteAllText(pathAndFileNamee, createText);
+
+
                 }
-           
+
             }
             catch (AmazonS3Exception e)
             {
@@ -194,5 +177,45 @@ namespace S3Test.services
                 Console.WriteLine("UnKnown encountered on server. Message:'{0}' when writing an object", e.Message);
             }
         }
+
+        long count = 0;
+
+        public long renderData(IRow headerRow, int cellCount, ISheet sheet)
+        {
+            for (int j = 0; j < cellCount; j++)
+            {
+                NPOI.SS.UserModel.ICell cell = headerRow.GetCell(j);
+                if (cell == null || string.IsNullOrWhiteSpace(cell.ToString())) continue;
+                var cel = cell.ToString();
+                count++;
+            }
+            for (int i = (sheet.FirstRowNum + 1); i <= sheet.LastRowNum; i++) //Read Excel File
+            {
+                IRow row = sheet.GetRow(i);
+                if (row == null) continue;
+                if (row.Cells.All(d => d.CellType == NPOI.SS.UserModel.CellType.Blank)) continue;
+                for (int j = row.FirstCellNum; j < cellCount; j++)
+                {
+                    if (row.GetCell(j) != null)
+                    {
+                        var c = row.GetCell(j).ToString();
+                    }
+                    count++;
+                }
+            }
+            return count;
+        }
     }
 }
+
+
+//Spire.Xls.Workbook workbook = new Spire.Xls.Workbook();
+//workbook.LoadFromStream(memStream);
+//var sheett = workbook.Worksheets.Count;
+//var row = workbook.Worksheets[2];
+//var rows = workbook.Worksheets[1];
+//var rowss = workbook.Worksheets[3];
+//var r = row.SelectionCount;
+//var dd = row.GetRowHeight(2);
+// var atr =      row.Range["A6"].Value;
+//var atr2 = row.Range["A7"].Value;
---
+++ S3Test/services/SpireExcel.cs
@@ -0,0 +1,32 @@
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace S3Test.services
+{
+    public sealed class SpireExcel 
+    {
+        public Spire.Xls.Workbook workbook = null;
+        public Spire.Xls.Worksheet WorkSheetName = null;
+
+     
+        public SpireExcel()
+        {
+            try
+            {
+                
+               //Spire.License.LicenseProvider.SetLicenseKey("ErZT6NpjAQACkAXlvPksKKblif6+wabU+q4CqcwGsW/fhXY2OdsVVPLMO4W/5jgekQPJC8hEomyBnFSwubLJ2yvf2bXalgwWwYPm8yEr7sXdbC2wDkjd2yrBDzw5M3qSAgT68GD97/+FA+X3ApbJxqOC1zLzt3lSOEcmacpCoW2FOwVgwt942c7SA8LQYYmOqfvQIUeimLnuDH3WiSCzjqArbay5d7PvwnI+mBkrHvnrn/5lQR3xBrKgQmrWdmWPFokTSn2T7mCl+gwbO3jw75xg+p4IWekIfladd1jjpfAY+6njX359TEKT+S3d3ubAdWeV255IunEKWN2pEg9rxCxUOJpvQIg4KmP3mHqp0Qj7NdRIXwOLiClqHBRZk5Wjr7aQwNgDiKosUMZwxzhPsKQjmrqDT6Hl1R0hlGQljGvfqUeAm6VJj1ZohfxVqsrLobDZAhGv7drrN2f4IhDkBz3QouO/UWpUGU/vJDowZwH8dwzJZVOvQ8dpqm70pH3n+DEjlHvg+PYghJipihUPGX/bQYoMYjW332bG2h5T24rah+NSSqtn2XCYi1T70PqCbtHxqXYh9ARyEspcEn2NKuV398PF2zTraNtYNRAeqiQlxaSo1j4cIRKTL3J432eGmaGJfAK9k/T/d3JKilP4McqTPkVLDHXlBJ5yb0i6LTyM4h15p2SyXgvlbBFdODdR68d+/9CbZaMgbOz9tuISfnJiZsh0EJdJsg6Oj7g8B1XDHu/TE5q1LSTUDN5RVhecQInSLnyPFNEeJuXnK0KGgeaTs4Rs54mTMqYoh2Iz5MjdzeTs5/E0dNaS63p3iBL64bcQaQVc4YvIXcHi6ywmiHg/PMvuHQb7qSUPMsBk7xc/QwAL2IKA6YCZxWl/qESIPHGC+GlVSZAw7xerA7/eE+xwU26173ytEPNflg3XrqAboHakIYGCb0/51cGCwJuQs8MksYWQQNniRofBCTX6rwOdjkw0u0IkTgxQ4FQ2JP3x1UmYFNDwLBGM/HZpaoSmQsCLQ091ZQWVU2xYZdV4vpz2Eocf8zffdrbxwju+wFBovhn9mXgGMmpIhfU2QMxY1Xgp9W+zTZHnbz0pME1IE2kzD0zpX9YXdAEg9149UKb06ecFwJSY8HP1BUR+WePw6KBw/ZSw3mYYCHbOT1DP9MFv/ibyKAxm0nmHwCoIV0Txw+rad9h8H45+ASBegJXmOrqCiZaFI96TMdfeejL2YcoLUViIMNQvLcaMoJWjarW/2MyhjI6vcjKTq1uY+ERLDbGYca2MygQybPYZUVcHLKUlcK1GiWSWWLFrY+AY2pTDqqNWv9QzYmzeSEqaAh6wR+8wWXh+HgVk9v+uGLrIK4Ql1yDBz+gP6VooVuf0wZFGO+i+Dg81dYjMB0/q5qfnkqpvvYHmAA3ub0njylAtqg88B98vfB4kHMqhjs2U/X6PMieRQe+08/pQYNnRrf1f+zUXMJGjxv9FNyszCSEnWQqwpX/MTnh7WWtt6EFhtQcayCbEaUlpIdqA92UUh/vN");
+                workbook = new Spire.Xls.Workbook();
+                
+                    
+             
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex);
+            }
+        }
+    }
+}
